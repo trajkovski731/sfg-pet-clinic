e24bee669dbf2a3f52560cf04c9a0955a4267913
filip.trajkovski <filip.trajkovski@sanitas.com>
2022-11-07 20:27:53 +0100

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/trajkovski/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.trajkovski.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/trajkovski/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.trajkovski.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
