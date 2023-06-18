6ce26d1332af1c1c57f557a22f617e1e55cab67a
filip.trajkovski <filip.trajkovski@sanitas.com>
2023-06-18 18:52:59 +0200

    Added ScheduledBranch

---
+++ pet-clinic-web/src/main/java/com/trajkovski/sfgpetclinic/controller/ScheduledBranch.java
@@ -0,0 +1,13 @@
+package com.trajkovski.sfgpetclinic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ScheduledBranch {
+
+    @GetMapping("/schedule")
+    public void print() {
+        System.out.println("Print something");
+    }
+}
